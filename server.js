490bf60bd553e136f04db2c6212deadf26765faa
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2022-05-09 13:53:17 -0700

    day 2.1

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './client/build')));
+}
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
