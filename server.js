329027454c9c6fb3143a2db0b525161c08b750db
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2022-05-12 13:48:27 -0700

    set static folder

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  //set statc folder
-  app.use(express.static(path.join(__dirname, './client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
-//
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
